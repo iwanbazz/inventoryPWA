87761233be4796cd29199b00c21ede60eec4049c
iwanbazz <iwan.bazz@gmail.com>
2020-06-08 05:37:38 +0700

    Change footer

--- src/components/Navigation/Footer/index.jsx
+++ src/components/Navigation/Footer/index.jsx
@@ -7,16 +7,21 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container-fluid">
-        <div className={classNames("level", classes.level)}>
+        <div className={classNames('level', classes.level)}>
           <div className="level-left">
             <div className="level-item">
-              © 2020 <span>&nbsp; CreateThrive</span>
+              © 2020 <span>&nbsp;</span>
             </div>
           </div>
-          <div className={classNames("level-right", classes.levelRight)}>
+          <div className={classNames('level-right', classes.levelRight)}>
             <div className="level-item">
               <div className="logo">
-                <a href="https://createthrive.com"><img src="https://createthrive.com/assets/logos/Logo-CT.svg" alt="CreateThrive.com" /></a>
+                <a href="https://github.com/iwanbazz/inventoryPWA">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+                    alt="https://github.com/iwanbazz/inventoryPWA"
+                  />
+                </a>
               </div>
             </div>
           </div>
